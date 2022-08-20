ef8ad54d1dbc09014135b83404ff574b13a22330
hieuhuule <hieuhuule@gmail.com>
2022-08-20 12:24:35 -0500

    completed assessment

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -1,5 +1,10 @@
 // ASSESSMENT 3: Coding Practical Questions with Jest
 
+// const { it } = require("node:test")
+
+// const { it } = require("node:test")
+// const { describe } = require("yargs")
+
 // Please read all questions thoroughly
 // Pseudo coding is REQUIRED
 // If you get stuck, please leave comments to help us understand your thought process
@@ -13,6 +18,17 @@
 
 // --------------------1) Create a function that takes in a number (greater than 2) and returns an array that length containing the numbers of the Fibonacci sequence.
 
+// PSUEDO CODE
+// create a function that takes a number greater than 2
+// will need a conditional to make sure it's a number greater than two
+// create an array to hold the numbers
+// create a for loop with the counter going up to the value of the number
+// create 3 variables, n1, n2, nextNum
+// value of nextNum = the values of n1 + n2
+// push the value of nextNum to array
+// reassign the value of n2 to n1
+// reassign the value of nextNum to n2
+
 // a) Create a test with expect statements for each of the variables provided.
 
 const fibLength1 = 6
@@ -21,11 +37,80 @@ const fibLength1 = 6
 const fibLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
+// Name the function
+describe("fibonacci", () => {
+    // What it does
+    it("takes a number and returns a fibonacci sequence of that length", () => {
+        // What do you expect the return to be given these tests
+        expect(fibonacci(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+        expect(fibonacci(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+    })
+})
+
+// yarn run v1.22.19
+// warning package.json: No license field
+// $ /home/gizmo/Development/personal-projects/practice/week-3-assessment-Hieuhuule/node_modules/.bin/jest
+//  FAIL  ./code-challenges.test.js
+//   fibonacci
+//     ✕ takes a number and returns a fibonacci sequence of that length (1 ms)
+
+//   ● fibonacci › takes a number and returns a fibonacci sequence of that length
+
+//     ReferenceError: fibonacci is not defined
+
+//       39 |     it("takes a number and returns a fibonacci sequence of that length", () => {
+//       40 |         // What do you expect the return to be given these tests
+//     > 41 |         expect(fibonacci(fibLength1)).toEqual([1, 1, 2, 3, 5, 8])
+//          |         ^
+//       42 |         expect(fibonacci(fibLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+//       43 |     })
+//       44 | }) 
+
+//       at Object.expect (code-challenges.test.js:41:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 total
+// Snapshots:   0 total
+// Time:        0.894 s
+// Ran all test suites.
+// error Command failed with exit code 1.
+// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+
 
 // b) Create the function that makes the test pass.
 
 
+const fibonacci = (num) => {
+    let n1 = 0;
+    let n2 = 1;
+    let nextNum = 0;
+    let newArr = [1] // starts at 1 so the program works
+
+    // covers edge case
+    if (num > 2) {
+        for (let i = 1; i < num; i++) {
+            // at start nextNum = 0
+            nextNum = n1 + n2
+            // adds the value of nextNum to newArr
+            newArr.push(nextNum)
+            // reassigns the value n1 to the value of n2
+            n1 = n2 // n1 = 1
+            // reassigns n2 the value of nextNum
+            n2 = nextNum
+        }
+        return newArr
+    }
+}
+
+
 // --------------------2) Create a function that takes in an array and returns a new array of only odd numbers sorted from least to greatest.
+
+//PSEUDO CODE
+// create a function that takes an array
+// iterate through array to get all the numbers
+// iterate through the numbers to get only the odd numbers
+// sort the odd numbers least to greatest
+// return the sorted array
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -36,10 +121,74 @@ const fullArr2 = ["hello", 7, 23, -823, false, 78, null, "67", 6, "number"]
 // Expected output: [-823, 7, 23]
 
 
+describe("sortOddLeastToGreatest", () => {
+    it("takes an array of different datatypes and returns an array of only odd numbers sorted least to greatest", () => {
+        expect(sortOddLeastToGreatest(fullArr1)).toEqual([-9, 7, 9, 199])
+        expect(sortOddLeastToGreatest(fullArr2)).toEqual([-823, 7, 23])
+    })
+})
+
+// yarn run v1.22.19
+// warning package.json: No license field
+// $ /home/gizmo/Development/personal-projects/practice/week-3-assessment-Hieuhuule/node_modules/.bin/jest
+// FAIL  ./code-challenges.test.js
+// fibonacci
+//     ✓ takes a number and returns a fibonacci sequence of that length (4 ms)
+// sortOddLeastToGreatest
+//     ✕ takes an array of different datatypes and returns an array of only odd numbers sorted least to greatest
+
+// ● sortOddLeastToGreatest › takes an array of different datatypes and returns an array of only odd numbers sorted least to greatest
+
+//     ReferenceError: sortOddLeastToGreatest is not defined
+
+//     120 | describe("sortOddLeastToGreatest", () => {
+//     121 |     it("takes an array of different datatypes and returns an array of only odd numbers sorted least to greatest", () => {
+//     > 122 |         expect(sortOddLeastToGreatest(fullArr1)).toEqual([-9, 7, 9, 199])
+//         |         ^
+//     123 |         expect(sortOddLeastToGreatest(fullAr2)).toEqual([-823, 7, 23])
+//     124 |     })
+//     125 | })
+
+//     at Object.expect (code-challenges.test.js:122:9)
+
+// Test Suites: 1 failed, 1 total
+// Tests:       1 failed, 1 passed, 2 total
+// Snapshots:   0 total
+// Time:        0.934 s
+// Ran all test suites.
+// error Command failed with exit code 1.
+// info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+
+
 // b) Create the function that makes the test pass.
+
+const sortOddLeastToGreatest = (array) => {
+    let numArr = []
+    let oddNums = []
+    let leastToGreatest = []
+    array.map(value => {
+        if (typeof value === "number") {
+            numArr.push(value) // iterates to get all the numbers
+        }
+    })
+    numArr.map(value => {
+        if (value % 2 !== 0) {
+            oddNums.push(value); // iterates to get all the odd numbers
+        }
+    })
+    leastToGreatest = oddNums.sort((a, b) => a - b) // sorts the numbers least to greatest
+    return leastToGreatest
+}
 
 
 // --------------------3) Create a function that takes in an array and returns an array of the accumulating sum. An empty array should return an empty array.
+
+// PSEUDO CODE
+// create a function that takes an arry of numbers
+// returns an array of accumulating sum
+// edge case for empty array
+// code similiar to fibonacci
+
 
 // a) Create a test with expect statements for each of the variables provided.
 
@@ -52,5 +201,70 @@ const numbersToAdd2 = [0, 7, -8, 12]
 const numbersToAdd3 = []
 // Expected output: []
 
+describe("accumulatingSum", () => {
+    it("takes in an array and returns an array of the accumulating sum", () => {
+        expect(accumulatingSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        expect(accumulatingSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        expect(accumulatingSum(numbersToAdd3)).toEqual([])
+    })
+})
+
+        //     yarn run v1.22.19
+        // warning package.json: No license field
+        // $ / home / gizmo / Development / personal - projects / practice / week - 3 - assessment - Hieuhuule / node_modules /.bin / jest
+        // FAIL./ code - challenges.test.js
+        // fibonacci
+        //     ✓ takes a number and returns a fibonacci sequence of that length(3 ms)
+        // sortOddLeastToGreatest
+        //     ✓ takes an array of different datatypes and returns an array of only odd numbers sorted least to greatest(1 ms)
+        // accumulatingSum
+        //     ✕ takes in an array and returns an array of the accumulating sum(1 ms)
+
+        // ● accumulatingSum › takes in an array and returns an array of the accumulating sum
+
+        // ReferenceError: accumulatingSum is not defined
+
+        // 204 | describe("accumulatingSum", () => {
+        //     205 | it("takes in an array and returns an array of the accumulating sum", () => {
+        //     > 206 | expect(accumulatingSum(numbersToAdd1)).toEqual([2, 6, 51, 60])
+        //             |             ^
+        //             207 | expect(accumulatingSum(numbersToAdd2)).toEqual([0, 7, -1, 11])
+        //         208 | expect(accumulatingSum(numbersToAdd3)).toEqual([])
+        //         209 |         })
+
+        //     at Object.expect(code - challenges.test.js: 206: 13)
+
+        // Test Suites: 1 failed, 1 total
+        //     Tests: 1 failed, 2 passed, 3 total
+        //     Snapshots: 0 total
+        //     Time: 0.91 s
+        // Ran all test suites.
+        // error Command failed with exit code 1.
+        // info Visit https://yarnpkg.com/en/docs/cli/run for documentation about this command.
+
 
 // b) Create the function that makes the test pass.
+
+const accumulatingSum = (arr) => {
+    let sum = []
+    let toAdd = []
+    let arrAndToAddSum = []
+
+    if (arr === []) { // edge case
+        return arr
+    } else {
+        sum.push(arr[0]) // captures first num in array outside of loop and assigns to "sum array"
+        for (let i = 1; i < arr.length; i++) { 
+            toAdd.push(arr[i]) // adds the arr[i] to an empty array called "toAdd"
+            lastItemInSum = sum[sum.length - 1] // assigns the last element in the "sum array" to lastItemInSum
+            toAdd.push(lastItemInSum) // puts arr[i] and "lastItemInSum" in the same array, "toAdd"
+            arrAndToAddSum = toAdd.reduce((a, b) => a + b) // adds all elements in "toAdd" and assigns the sum to "arrAndToAddSum", also clears the previous value of "arrAndToAddSum" from the previous loop
+            sum.push(arrAndToAddSum) // adds the value of "arrAndToAddSum" to end of "sum array"
+            toAdd = [] // empties toAdd array
+            // console.log("lastitem", lastItemInSum)
+        }
+        return sum
+    }
+}
+
+// This one was definitely very difficult for me to figure out, so much that I ended up creating this inelegant solution to it since I couldn't figure out how to accomplish it otherwise. What I don't like about it is that I couldn't figure out how to refactor it using HOF and that I was explicitly clearing out variables.
--- interview-questions.md
+++ interview-questions.md
@@ -8,48 +8,91 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: JSX is the coding language used by React. It's very similar to Javascript syntax with a few exceptions such as reserved class names. 
 
-Researched answer:
+Researched answer: JSX stands for Javascript XML. It allows you to write HTML in Javascript by converting HTML tags into React elements declaratively instead of imperatively, without the need for methods like createElement(). An example of how JSX is written declaratively vs imperatively is as follows:
+
+DECLARATIVE JSX: const myElement = <h1>This is JSX!</h1>
+IMPERATIVE: ReactDOM.render(myElement, document.getElementByID('root'))
+
+ref: https://medium.com/@sjarancio/what-is-jsx-e3dda0af3490
 
 2. What is yarn? What file(s) are modified in a React application when you run yarn?
 
-Your answer:
+Your answer: yarn stands for "yet another resource navigator", it's a package manager that is used to install packages and libraries on your local machine, as well as executing scripts to run code.
 
-Researched answer:
+Researched answer: yarn is a package manager for code, allowing you to share code with other developers quickly, securely, and reliably. Code is shared through something called a module or package. It was developed in 2016 by Facebook for the Node.js JavaScript runtime environment to solve consistency, security, and performance problems with large codebases.
+
+ref: https://classic.yarnpkg.com/lang/en/docs/getting-started/
+        https://en.wikipedia.org/wiki/Yarn_(package_manager) 
+
+Note to Charlean: it's not plagiarism if you include references :) 
 
 3. What is an anonymous function in JavaScript?
 
-Your answer:
+Your answer: I actually don't know much about this, I know it's an unnamed arrow function that you can pass as an argument when doing a component call.
 
-Researched answer:
+Researched answer: Anonymous functions are functions without names that are not accessible after it's initial creation unless you assign it to a variable. They are executed when the interpreter comes across them. This means that you cannot call them before the interpreter has discovered it. It also means that nay code that appears tup to that point could potentially alter what goes on inside of an anonymous function. Sometimes anonymous functions are called "immediately invoked function expressions (IIFE), pronounced "iffy". They are used for code that only needs to run once within a task, rather than repeatedly being called by other parts of the script. IIFEs are commonly used as a wrapper around a set of code. Any variables declared within that anonymous function are effectively protected from variables in other scripts that might have the same name. This is due to the concept of scope.
+
+ref: JavaScript & JQuery interacive front-end web development by Jon Duckett
 
 4. What is the difference between a class and an object?
 
-Your answer:
+Your answer: A class is a blueprint for an object, it has parameters that can be filled into easily create and replicate other instances of said object. An Object is a data structure that holds key value pairs in no specific order or arrangement, whereas an array stores data at indexes.
 
-Researched answer:
+Researched answer: A class is a template that defines object properties and behavior. An object is a member or "instance of a class", containing a state which has values you define or that are defined by default
+
+ref: https://www.ncl.ucar.edu/Document/HLUs/User_Guide/classes/classoview.shtml#:~:text=A%20class%20defines%20object%20properties,are%20defined%20by%20default%20settings.
 
 5. What is the difference between state and props in React?
 
-Your answer:
+Your answer: state exists in the file that it is created and it's data cannot explicitly move between files, however, props can move data between parent and child components. Props is an object with no explicit values unless assigned them.
 
-Researched answer:
+Researched answer: This will be provided in list form:
+    1. Props are Read-Only / State changes can be asynchronous
+    2. Props are immutable / State is mutable
+    3. Props allow you to pass data between components as an argument / State hold info about the components
+    4. Props can be accessed by the child component / State cannot be accessed by child components
+    5. Props are used to communicate between components / States can be used for rendering dynamic changes with the component
+    6. Stateless components can have Props / Stateless components cannot have State
+    7. Props make components reusable / State cannot make components reusable
+    8. Props are external and controlled by whatever renders the component / The State is internal and controlled by the React component itself.
+
+ref: https://www.javatpoint.com/react-state-vs-props
 
 6. STRETCH: Which is the difference between a div and a span?
 
-Your answer:
+Your answer: I don't know, I haven't used span very much at all.
 
-Researched answer:
+Researched answer: div and span are both html elements that group together related parts of a web page. However, they serve different functions. A div element is used for block-level organization and styling of page elements, while span is used for inline organization and styling. Span tags are used on small segments of text, links, images, and other HTML elements that appear inline with the surrounding content. Here are examples of both:
+
+DIV: <div id=“paragraphs”>
+    <p>This is my first paragraph.</p>
+    <p>This is my second paragraph.</p>
+    <p>This is my final paragraph.</p>    
+    </div>
+
+SPAN: <p>This is a paragraph with <span id=“special-text”>a little something extra</span> inside it.</p>
+
+ref: https://blog.hubspot.com/website/span-vs-div#:~:text=div%20in%20HTML,for%20inline%20organization%20and%20styling.
 
 ## Looking Ahead: Terms for Next Week
 
-1. Object-oriented programming:
+1. Object-oriented programming: Object-oriented programming (OOP) is a type of programming based on the concept of "objects", which can contain data, in the form of fields (often known as attributes or properties), and code, in the form of procedures (often known as methods). OOP is all about being able to structure code so that its functionality can be shared throughout the application. This is opposed to procedural programming (PP), in which you build programs in sequential order and call methods when you want shared behavior between pages in the application. 
 
-2. Ruby:
+ref: Flatiron School, Introduction to Object Orientation in Ruby
 
-3. Implicit return:
+2. Ruby: Ruby is a powerful object-oriented language used by popular applications. Ruby was built to be easy for you to read and understand. Ruby was built to help you tell stories that computers and human beings can both enjoy, and so instead of weird symbols or words like "static" and "void", you programs that look almost like English, with words like "unless", "rescue", "self", and even "begin" and "end"
 
-4. Ruby blocks:
+ref: Ruby Wizardry from No Starch Press
 
-5. Ruby hashes:
+3. Implicit return: Every method in Ruby returns a value by default. This returned value will be the value of the last statement.
+
+ref: Flatiron School
+
+4. Ruby blocks: A block is a bit of code enclosed in do/end keywords or curly brackets ({}). We call a method with a block by simply appending the block at the end of the method call.
+
+ref: Flatiron School, Yield and Blocks
+
+5. Ruby hashes: A data structure that holds key/value pairs
+
